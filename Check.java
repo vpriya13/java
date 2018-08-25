6be31b483ede129ff1e1a87716f1244c1742987c
vpriya13 <42690248+vpriya13@users.noreply.github.com>
2018-08-25 12:36:43 +0530

    Update and rename README.md to Check.java

---
+++ Check.java
@@ -0,0 +1,13 @@
+public class Check {
+
+    public static void main(String[] args) {
+
+        char ch = 'e';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
