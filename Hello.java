5d6e8aa6ef96f35f2c8cc9250bc55f036f1a59da
vpriya13 <42690248+vpriya13@users.noreply.github.com>
2018-08-28 18:09:42 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package practice;
+import java.util.*;
+public class Hello {
+	      public static void main(String[] args) {
+
+	            System.out.println("Enter The Number.");
+	            Scanner sc=new Scanner(System.in);
+              int n=sc.nextInt();
+	            int i;
+	            for (i = n; i < 100; i++)
+	                  System.out.println("Hello!");
+	            System.out.println("Now the code looks much better.");
+	      }
+}
