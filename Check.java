cc203a50967dfc8f7fa95d14b1f61cbd6b0b4001
vpriya13 <42690248+vpriya13@users.noreply.github.com>
2018-08-28 17:02:47 +0530

    Update Check.java

--- Check.java
+++ Check.java
@@ -2,7 +2,7 @@ public class Check {
 
     public static void main(String[] args) {
 
-        char ch = 'e';
+        char ch = 'a';
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
