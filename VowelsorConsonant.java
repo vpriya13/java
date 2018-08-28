ba1ff58ca2f2303247cdcc492ed6342320a048e4
vpriya13 <42690248+vpriya13@users.noreply.github.com>
2018-08-28 17:04:27 +0530

    Create  VowelsorConsonant.java

---
+++ VowelsorConsonant.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class VowelsorConsonant {
+		    public static void main(String[] args) {
+
+	        char ch = 'a';
+
+	        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+	            System.out.println(ch + " is vowel");
+	        else
+	            System.out.println(ch + " is consonant");
+
+	    }
+	}
+
