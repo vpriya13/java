c7a6d90554d1f6ce3d3ff4b569091de790ecc9a1
vpriya13 <42690248+vpriya13@users.noreply.github.com>
2018-09-03 17:58:39 +0530

    Update Power.java

--- Power.java
+++ Power.java
@@ -2,13 +2,13 @@ package practice;
 import java.util.Scanner;
 public class Power {
 	public static void main(String[] args) {
-    int base, exponent, result;
+    int b, e, result;
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter a base number: ");
     base=sc.nextInt();
     System.out.println("Enter an exponent: ");
     exponent=sc.nextInt();
     // calculates the power
-    System.out.println(Math.pow(base,exponent));
+    System.out.println(Math.pow(b,e));
 }
 }
