b35c2b21a67ed5dec6d8d03c71021f1abb54793f
vpriya13 <42690248+vpriya13@users.noreply.github.com>
2018-09-12 18:16:49 +0530

    Create ArrayN.java

---
+++ ArrayN.java
@@ -0,0 +1,14 @@
+import java.util.*
+Public class ArrayN{
+public static void main(String args[]){
+  int i=0,n=0;
+  System.out.println("enter the limit of n:");
+  Scanner sc=new Scanner(System.in);
+  n=sc.nextInt();
+  int a[]=new int[n];
+    for(i=0;i<n;i++){
+      a[i]=sc.nextInt();
+}
+System.out.println("the numbers are;" +a[i]);
+}
+}
