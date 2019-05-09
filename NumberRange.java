985ab5ac2b7f6a6cfa5dd561497308e426565fa1
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-05-09 15:29:38 +0530

    Create NumberRange.java

---
+++ NumberRange.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    if(n>1&&n<10){
+		System.out.println("yes");
+	    }
+	    else
+	    System.out.println("no");
+	}
+}
