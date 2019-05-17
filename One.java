ced31c3935b467282ac35e3291e7ba025d41db69
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-05-17 10:30:38 +0530

    Create One.java

---
+++ One.java
@@ -0,0 +1,47 @@
+package guvi;
+import java.util.*;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int num=sc.nextInt();
+		switch(num) {
+			case 1:
+				System.out.println("one");
+				break;
+			case 2:
+				System.out.println("two");
+				break;
+			case 3:
+				System.out.println("three");
+				break;
+			case 4:
+				System.out.println("four");
+				break;
+			case 5:
+				System.out.println("five");
+				break;
+			case 6:
+				System.out.println("six");
+				break;
+			case 7:
+				System.out.println("seven");
+				break;
+			case 8:
+				System.out.println("eight");
+				break;
+			case 9:
+				System.out.println("nine");
+				break;
+			case 10:
+				System.out.println("ten");
+				break;
+			
+		}
+
+	}
+
+}
