ee9ec409ec9825f4d94832b2244002b46fbc7125
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-08 12:22:11 +0530

    Create WithoutSemicolon.java

---
+++ WithoutSemicolon.java
@@ -0,0 +1,14 @@
+package GUVI;
+import java.util.*;
+public class Without_Semicolon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String S=sc.nextLine();
+		if(System.out.printf(S)==null){
+			
+		}
+	}
+
+}
