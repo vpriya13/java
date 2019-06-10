6bf79b43f5f2d2ee3c11f638389bd20331d90e47
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-10 14:07:49 +0530

    Create MulOf7.java

---
+++ MulOf7.java
@@ -0,0 +1,17 @@
+package GUVI;
+import java.util.Scanner;
+public class MulOf7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[10];
+		if(n%7==0)
+		System.out.println("yes");
+		else
+			System.out.println("no");
+
+	}
+
+}
