bb4b923787a391f5d7a3fa118cc2eeb400943de9
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-10 14:56:26 +0530

    Create NBtwLR.java

---
+++ NBtwLR.java
@@ -0,0 +1,18 @@
+package GUVI;
+import java.util.*;
+public class NBtwLR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int l=sc.nextInt();
+		int r=sc.nextInt();
+		if(n>=l&&n<=r){
+			System.out.println("yes");
+		}
+		else
+			System.out.println("no");
+	}
+
+}
