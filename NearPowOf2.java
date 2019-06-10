b677fb460e97f944919620a731ac8ffd3719432f
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-10 14:16:10 +0530

    Create NearPowOf2.java

---
+++ NearPowOf2.java
@@ -0,0 +1,13 @@
+package GUVI;
+import java.util.Scanner;
+public class NearPowOf2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int p=n*2;
+		System.out.println(p);
+	}
+
+}
