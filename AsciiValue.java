cf9c1a50bb03c90cf8d09d1ec901ac9e594dd9e2
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-11 13:58:58 +0530

    Create AsciiValue.java

---
+++ AsciiValue.java
@@ -0,0 +1,13 @@
+package GUVI;
+import java.util.Scanner;
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		int c=ch;
+		System.out.println(c);
+	}
+
+}
