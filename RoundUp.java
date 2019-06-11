b52a3d43f476fa1d36631d88fd7ab5bd529d568e
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-11 12:30:16 +0530

    Create RoundUp.java

---
+++ RoundUp.java
@@ -0,0 +1,14 @@
+package GUVI;
+import java.util.Scanner;
+public class RoundUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		double d=sc.nextDouble();
+		
+        System.out.println(Math.round(d)); 
+
+	}
+
+}
