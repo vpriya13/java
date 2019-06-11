162cd28fb4f09941c264ef2bb208572a706a93dd
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-11 17:40:17 +0530

    Create ABModC.java

---
+++ ABModC.java
@@ -0,0 +1,18 @@
+package GUVI;
+
+import java.util.Scanner;
+
+public class ABModC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int cal=(a*b)%c;
+		System.out.println(cal);
+
+	}
+
+}
