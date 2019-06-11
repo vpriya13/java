bcda17b025666eddc0ebe359c93594dc5f8c1fe2
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-11 13:44:23 +0530

    Create Decimal5.java

---
+++ Decimal5.java
@@ -0,0 +1,15 @@
+package GUVI;
+import java.util.Scanner;
+
+public class Decimal5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		float length=sc.nextFloat();
+		float breadth=sc.nextFloat();
+		System.out.format("%5f",length*breadth);
+
+	}
+
+}
