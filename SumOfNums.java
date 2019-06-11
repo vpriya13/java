1368d7f8d63cda13a701ad278693fbbd30eef428
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-11 16:10:08 +0530

    Create SumOfNums.java

---
+++ SumOfNums.java
@@ -0,0 +1,22 @@
+package GUVI;
+
+import java.util.Scanner;
+
+public class SumOfNums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+			sum+=arr[i];
+		}
+		System.out.println(sum);
+		
+
+	}
+
+}
