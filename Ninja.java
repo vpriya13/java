fa3ae52b19050f02299f0a86482d7ba1a65d2c02
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-11 13:37:33 +0530

    Create Ninja.java

---
+++ Ninja.java
@@ -0,0 +1,14 @@
+package GUVI;
+import java.util.Scanner;
+public class Ninjas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1=sc.nextInt();
+		int mins=n1-n;
+		System.out.println(mins);
+	}
+
+}
