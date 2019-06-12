08a03561bc13c111c65f5171aeb1a2421f3441d9
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-12 11:53:52 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,17 @@
+package GUVISET11;
+
+import java.util.Scanner;
+
+public class Prg7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int C=sc.nextInt();
+		int Res=(A*B)/C;
+		System.out.println(Res);
+	}
+
+}
