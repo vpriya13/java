34e33c92b6316ba0cd282ecc3340e70354d69134
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-12 11:44:08 +0530

    Create Join.java

---
+++ Join.java
@@ -0,0 +1,16 @@
+package GUVISET11;
+
+import java.util.Scanner;
+
+public class Join {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+		System.out.println(s1+s2);
+
+	}
+
+}
