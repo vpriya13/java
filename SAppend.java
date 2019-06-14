670eafa95ffbc0dfbeef130a26619ae4f4a57e9f
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-06-14 17:48:00 +0530

    Create SAppend.java

---
+++ SAppend.java
@@ -0,0 +1,16 @@
+package GUVI_PLAYER;
+
+import java.util.Scanner;
+
+public class SAppend {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		// TODO Auto-generated method stub
+		StringBuilder str=new StringBuilder(s);
+		System.out.println(str.append("."));
+
+	}
+
+}
