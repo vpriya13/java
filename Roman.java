4926156be3ce37b758e575cf31c996ebd9dac4ea
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-07-26 19:10:10 +0530

    Update Roman.java

--- Roman.java
+++ Roman.java
@@ -38,22 +38,7 @@ public class Roman {
                                         previousnum = num;
                                 num = 10;
                                 break;
-                                case 'L':
-                                        previousnum = num;
-                                num = 50;
-                                break;
-                                case 'C':
-                                        previousnum = num;
-                                num = 100;
-                                break;
-                                case 'D':
-                                        previousnum = num;
-                                num = 500;
-                                break;
-                                case 'M':
-                                        previousnum = num;
-                                num = 1000;
-                                break;
+               
                         }           
                         if (num<previousnum)
                         {decimalNum= decimalNum-num;}
