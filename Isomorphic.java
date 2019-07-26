50552bea9671c0d7f77d2106c5024e314f919310
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-07-26 18:23:32 +0530

    Create Isomorphic.java

---
+++ Isomorphic.java
@@ -0,0 +1,41 @@
+package PRP;
+import java.util.*;
+public class Isomorphic {
+	
+	public boolean isIsomorphic(String s, String t) {
+	    if(s.length()!=t.length()){
+	        return false;
+	    }
+	 
+	    HashMap<Character, Character> map1 = new HashMap<>();
+	    HashMap<Character, Character> map2 = new HashMap<>();
+	 
+	    for(int i=0; i<s.length(); i++){
+	        char c1 = s.charAt(i);
+	        char c2 = t.charAt(i);
+	 
+	        if(map1.containsKey(c1)){
+	            if(c2!=map1.get(c1)){
+	                return false;
+	            }
+	        }else{
+	            if(map2.containsKey(c2)){
+	                return false;
+	            }
+	 
+	            map1.put(c1, c2);
+	            map2.put(c2, c1);
+	        }
+	    }
+	 
+	    return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Isomorphic i=new Isomorphic();
+		System.out.println(i.isIsomorphic("aab","xzy"));
+
+	}
+
+}
