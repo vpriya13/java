de3ab4457712c5e2a533673702a6df44f5b479c5
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-07-28 12:29:50 +0530

    Create RemoveAllSpace.java

---
+++ RemoveAllSpace.java
@@ -0,0 +1,16 @@
+package GUVI_PLAYER;
+
+import java.util.Scanner;
+
+
+public class TrimWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		System.out.println(str.replaceAll("\\s+",""));
+
+	}
+
+}
