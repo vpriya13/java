f5af6d14321bc188f74f8bd295c57e630ccf84b4
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-07-29 14:04:26 +0530

    Create StrCaseInsensitive.java

---
+++ StrCaseInsensitive.java
@@ -0,0 +1,19 @@
+package GUVI_PLAYER;
+
+import java.util.Scanner;
+
+public class Strlen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		String str1=sc.next();
+		if(str.equalsIgnoreCase(str1)){
+			System.out.println("yes");
+		}
+		else
+			System.out.println("no");
+	}
+
+}
