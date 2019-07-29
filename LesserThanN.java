cce754f88ee9aab5267ff4ce6893550c32da5691
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-07-29 14:54:47 +0530

    Create LesserThanN.java

---
+++ LesserThanN.java
@@ -0,0 +1,29 @@
+package GUVI_PLAYER;
+
+import java.util.Scanner;
+
+public class RepChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		int k=sc.nextInt();
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+		
+		int res=0;
+		
+			for(int i=0;i<a.length;i++){
+				if(a[i]<n){
+					System.out.print(a[i]+" ");
+				}
+			}
+		
+		
+	}
+
+}
