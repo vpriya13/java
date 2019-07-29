359cdc6093938b81be119a007ac5bd8bad02dc8d
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-07-29 12:35:42 +0530

    Create IntegerValue.java

---
+++ IntegerValue.java
@@ -0,0 +1,16 @@
+package GUVI_PLAYER;
+
+import java.util.Scanner;
+
+public class IsInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if (n >=-2147483648 && n <=2147483647)
+			System.out.println("int");
+
+	}
+
+}
