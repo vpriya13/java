d63e9350c6bbf88fa245b1a085a8b9aba29983c8
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-07-29 13:52:32 +0530

    Create KSmallestElement.java

---
+++ KSmallestElement.java
@@ -0,0 +1,23 @@
+package GUVI_PLAYER;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SecSmallest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		int secmin=arr[k];
+		System.out.println(secmin);
+
+	}
+
+}
