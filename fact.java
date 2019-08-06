21b8a360d089f0decba37bf3309c8b459a4fe038
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-08-06 16:08:16 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int mul=1;
+        for(int i=1;i<=n;i++){
+            mul*=i;
+        }
+        System.out.println(mul);
+    }
+}
