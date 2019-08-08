bc0c25c7b9009083514d130601a507ae681b1363
V Mohana priya <42690248+vpriya13@users.noreply.github.com>
2019-08-08 17:51:01 +0530

    Create GuviAns.java

---
+++ GuviAns.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        
+      Scanner sb=new Scanner(System.in);
+      String s=sb.next();
+      StringBuilder sc=new StringBuilder(s+" ");
+      
+      System.out.println(sc.append("Answer"));
+    }
+}
